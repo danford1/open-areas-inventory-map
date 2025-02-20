012954532c4eed5a0ff8bbfbfedc91abc4b7d152
Dan Ford <dan@dendroyka.com>
2025-02-20 00:09:59 -0500

    add shoelace component library to main.js

--- src/main.js
+++ src/main.js
@@ -3,3 +3,9 @@ import './app.less'
 import App from './App.vue'
 
 createApp(App).mount('#app')
+
+import '@shoelace-style/shoelace/dist/themes/light.css';
+import { setBasePath } from '@shoelace-style/shoelace/dist/utilities/base-path';
+
+setBasePath('https://cdn.jsdelivr.net/npm/@shoelace-style/shoelace@2.20.0/cdn/');
+
